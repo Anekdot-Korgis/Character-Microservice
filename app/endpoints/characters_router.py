7be2fb459d6afdc659801d0e4df06a376845a911
Ilya Shishov <ilya.shishov2016@yandex.ru>
2023-12-12 22:34:45 +0300

    Add router boilerplate

--- app/endpoints/characters_router.py
+++ app/endpoints/characters_router.py
@@ -3,5 +3,20 @@ from fastapi import APIRouter, Depends, HTTPException, Body
 from app.models.character import Character
 from app.models.equipment import Equipment
 
+# todo: from app.services.servicename import CharactersService
+
 characters_router = APIRouter(prefix='/characters', tags=['Characters'])
 
+@characters_router.get('/')
+def get_characters():
+    return None # сходить в сервис и получить список чопиксов
+
+@characters_router.post('/create_character')
+def create_character(
+    character: Character,
+    # todo: characters_service: Depends(CharactersService)
+):
+    try:
+        return None # сходить в сервис и создать чопикса
+    except:
+        raise HTTPException(418, 'Я чайник')
