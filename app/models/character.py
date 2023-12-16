7864788061b7fa20e00241613a7c8cae482ca848
Big Boss <stulk7746@gmail.com>
2023-12-16 18:51:14 +0300

    COMPLETE: UUID

--- app/models/character.py
+++ app/models/character.py
@@ -6,15 +6,14 @@ from pydantic import BaseModel, Field, ConfigDict
 from app.models.equipment import Equipment
 
 class Character(BaseModel):
-  model_config = ConfigDict(from_attributes=True)
+ model_config = ConfigDict(from_attributes=True)
 
-  id: Annotated[UUID, Field(default_factory=lambda: UUID(int=0))]
-  name: str
-  class_name: str
-  level: int
-  experience: int
-  strength: int
-  agility: int
-  intelligence: int
-  luck: int
-
+ id: Annotated[UUID, Field(default_factory=lambda: UUID(int=0), exclude=True)]
+ name: str
+ class_name: str
+ level: int
+ experience: int
+ strength: int
+ agility: int
+ intelligence: int
+ luck: int
