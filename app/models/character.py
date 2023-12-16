5f970f983023708acf786e2b3860fd654c978e81
Big Boss <stulk7746@gmail.com>
2023-12-16 15:41:47 +0300

    ADD: UUID to character+equipment

--- app/models/character.py
+++ app/models/character.py
@@ -1,19 +1,20 @@
 # /app/models/character.py
 
-from pydantic import BaseModel, ConfigDict
-
+from uuid import UUID
+from typing_extensions import Annotated
+from pydantic import BaseModel, Field, ConfigDict
 from app.models.equipment import Equipment
 
-
 class Character(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
+  model_config = ConfigDict(from_attributes=True)
 
-    id: int
-    name: str
-    class_name: str
-    level: int
-    experience: int
-    strength: int
-    agility: int
-    intelligence: int
-    luck: int
+  id: Annotated[UUID, Field(default_factory=lambda: UUID(int=0))]
+  name: str
+  class_name: str
+  level: int
+  experience: int
+  strength: int
+  agility: int
+  intelligence: int
+  luck: int
+
--- app/schemas/character.py
+++ app/schemas/character.py
@@ -1,17 +1,19 @@
 # /app/schemas/character.py
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.dialects.postgresql import UUID
 from app.schemas.base_schema import Base
 
 class Character(Base):
-    __tablename__ = 'character'
+   __tablename__ = 'character'
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    class_name = Column(String)
-    level = Column(Integer)
-    experience = Column(Integer)
-    strength = Column(Integer)
-    agility = Column(Integer)
-    intelligence = Column(Integer)
-    luck = Column(Integer)
+   id = Column(UUID(as_uuid=True), primary_key=True, index=True)
+   name = Column(String)
+   class_name = Column(String)
+   level = Column(Integer)
+   experience = Column(Integer)
+   strength = Column(Integer)
+   agility = Column(Integer)
+   intelligence = Column(Integer)
+   luck = Column(Integer)
+
