6f7bdbc80cba318fa386bd5f735a414a4f2a187c
Big Boss <stulk7746@gmail.com>
2023-12-19 14:20:09 +0300

    если это не последний коммит я ебанусь блядь

--- tests/e2e/test_character_router.py
+++ tests/e2e/test_character_router.py
@@ -13,15 +13,15 @@ def test_get_characters():
 
 def test_create_character():
  character = {
-   'id': str(uuid.uuid4()),
-   'name': 'test',
-   'class_name': 'test',
-   'level': 1,
-   'experience': 0,
-   'strength': 0,
-   'agility': 0,
-   'intelligence': 0,
-   'luck': 0
+  'id': str(uuid.uuid4()),
+  'name': 'test',
+  'class_name': 'test',
+  'level': 1,
+  'experience': 0,
+  'strength': 0,
+  'agility': 0,
+  'intelligence': 0,
+  'luck': 0
  }
  response = client.post('/characters/create_character', json=character)
  assert response.status_code == 200
--- tests/unit/test_character_models.py
+++ tests/unit/test_character_models.py
@@ -6,7 +6,7 @@ import uuid
 
 def test_character_model():
  character = Character(id=str(uuid.uuid4()), name='test', class_name='test', level=1, experience=0, strength=0, agility=0, intelligence=0, luck=0)
- assert character.id == str(uuid.uuid4())
+ assert isinstance(character.id, uuid.UUID)
  assert character.name == 'test'
  assert character.class_name == 'test'
  assert character.level == 1
@@ -18,10 +18,10 @@ def test_character_model():
 
 def test_equipment_model():
  equipment = Equipment(id=str(uuid.uuid4()), name='test', strength_impact=0, dex_impact=0, intelligence_impact=0, luck_impact=0, character_id=str(uuid.uuid4()))
- assert equipment.id == str(uuid.uuid4())
+ assert isinstance(equipment.id, uuid.UUID)
  assert equipment.name == 'test'
  assert equipment.strength_impact == 0
  assert equipment.dex_impact == 0
  assert equipment.intelligence_impact == 0
  assert equipment.luck_impact == 0
- assert equipment.character_id == str(uuid.uuid4())
+ assert isinstance(equipment.character_id, uuid.UUID)
