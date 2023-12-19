5b1aa43bb948e8e2122cb74aa67899afe2f8c631
Big Boss <stulk7746@gmail.com>
2023-12-19 15:30:03 +0300

    Update test_character_router.py

--- tests/e2e/test_character_router.py
+++ tests/e2e/test_character_router.py
@@ -1,31 +1,23 @@
 import pytest
+from main import app
 from fastapi.testclient import TestClient
-import app
+from app.models.character import Character
+from app.repositories.character_repo import CharacterRepo
 import uuid
 
 client = TestClient(app)
 
-def test_create_character():
-   character = {
-       'id': str(uuid.uuid4()),
-       'name': 'test',
-       'class_name': 'test',
-       'level': 1,
-       'experience': 0,
-       'strength': 0,
-       'agility': 0,
-       'intelligence': 0,
-       'luck': 0
-   }
-   response = client.post('/characters/create_character', json=character)
-   assert response.status_code == 200
-   assert response.json() == {
-       'name': character['name'],
-       'class_name': character['class_name'],
-       'level': character['level'],
-       'experience': character['experience'],
-       'strength': character['strength'],
-       'agility': character['agility'],
-       'intelligence': character['intelligence'],
-       'luck': character['luck']
-   }
+def test_character_repo():
+ repo = CharacterRepo()
+ characters = repo.get_characters()
+ assert isinstance(characters, list)
+ character = repo.create_character(Character(id=str(uuid.uuid4()), name='test', class_name='test', level=1, experience=0, strength=0, agility=0, intelligence=0, luck=0))
+ assert isinstance(character.id, uuid.UUID)
+ assert character.name == 'test'
+ assert character.class_name == 'test'
+ assert character.level == 1
+ assert character.experience == 0
+ assert character.strength == 0
+ assert character.agility == 0
+ assert character.intelligence == 0
+ assert character.luck == 0
