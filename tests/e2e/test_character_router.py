44c0c241696156b4a70fddde870569eaa1a8fc67
Big Boss <stulk7746@gmail.com>
2023-12-19 15:00:07 +0300

    Update test_character_router.py

--- tests/e2e/test_character_router.py
+++ tests/e2e/test_character_router.py
@@ -1,28 +1,31 @@
-# /tests/e2e/test_character_router.py
-
+import pytest
 from fastapi.testclient import TestClient
-from app.endpoints.character_router import characters_router
+from main import app
 import uuid
 
-client = TestClient(characters_router)
-
-def test_get_characters():
- response = client.get('/characters')
- assert response.status_code == 200
- assert isinstance(response.json(), list)
+client = TestClient(app)
 
 def test_create_character():
- character = {
-  'id': str(uuid.uuid4()),
-  'name': 'test',
-  'class_name': 'test',
-  'level': 1,
-  'experience': 0,
-  'strength': 0,
-  'agility': 0,
-  'intelligence': 0,
-  'luck': 0
- }
- response = client.post('/characters/create_character', json=character)
- assert response.status_code == 200
- assert response.json() == character
+   character = {
+       'id': str(uuid.uuid4()),
+       'name': 'test',
+       'class_name': 'test',
+       'level': 1,
+       'experience': 0,
+       'strength': 0,
+       'agility': 0,
+       'intelligence': 0,
+       'luck': 0
+   }
+   response = client.post('/characters/create_character', json=character)
+   assert response.status_code == 200
+   assert response.json() == {
+       'name': character['name'],
+       'class_name': character['class_name'],
+       'level': character['level'],
+       'experience': character['experience'],
+       'strength': character['strength'],
+       'agility': character['agility'],
+       'intelligence': character['intelligence'],
+       'luck': character['luck']
+   }
